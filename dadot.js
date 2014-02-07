9c14b66f58ff73c8d2578cc4e26118eaae7d0d13
momamene <momamene@gmail.com>
2014-02-07 16:32:29 +0900

    dadot don't use http request

--- dadot.js
+++ dadot.js
@@ -1,7 +1,5 @@
 var jsdom = require('jsdom'),
-    _ = require('underscore'),
-    Iconv = require('iconv').Iconv,
-    euckr2utf8 = new Iconv('EUC-KR', 'UTF-8//TRANSLIT//IGNORE');
+    _ = require('underscore');
 
 var getArticle = function(window) {
     var $ = window.jQuery;
@@ -30,32 +28,12 @@ var getArticle = function(window) {
 var Dadot = module.exports = {
     extract: function(options) {
         var jsdom_env = {
-            url: options.url,
-            scripts: ['//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'],
+            html: options.html,
+            scripts: ['lib/jquery.min.js'],
             done: function (err, window) {
-                console.log(window.document.documentElement.innerHTML
-                            .replace(/<!--(.|\s)*?-->/gi,"")
-                            .replace(/\r?\n|\r/gi,"") // remove enter key (carrage returen)
-                            .replace(/\t/gi,"")
-                            .replace(/&nbsp;/," ")
-                            .replace(/<style[\w\W]+<\/style>/gi,"") // remove <style>
-                            .replace(/<script[\w\W]+<\/script>/gi,"") // remove <script>
-                            .replace(/<[^\>]+>/gi," ")); // remove other tag
                 options.done(getArticle(window));
             }
         };
-        if (options.charset.toUpperCase() === 'EUC-KR') {
-            jsdom_env = _.extend(jsdom_env, {
-                encoding: 'binary',
-                done: function(err, window) {
-                    var text = window.document.documentElement.innerHTML;
-                    var buf = new Buffer(text.length);
-                    buf.write(text, 0, text.length, 'binary');
-                    window.document.documentElement.innerHTML = euckr2utf8.convert(buf).toString();
-                    options.done(getArticle(window));
-                }
-            });
-        }
         jsdom.env(jsdom_env);
     }
 }
